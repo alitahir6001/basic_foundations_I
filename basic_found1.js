eda91c372fb14bec6dda13c1aecb51ef6721b095
Ali Tahir <alitahir6001@gmail.com>
2020-11-23 15:22:38 -0500

    cleaning up line spaces

--- basic_found1.js
+++ basic_found1.js
@@ -143,6 +143,7 @@ function replacenegatives(arr) {
 console.log(replacenegatives([1,5,10,-2]));
 
 
+
 // 11. Max/Min/Avg - Given an array with multiple values, write a function that returns a new array that only contains the maximum, minimum, and average values of the original array. (e.g. [1,5,10,-2] will return [10,-2,3.5])
 
 function minimaxavg(arr) {
@@ -150,7 +151,6 @@ function minimaxavg(arr) {
     var min = arr[0];
     var sum = 0;
     var avg = 0;
-
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] > max) {
             max = arr[i]
